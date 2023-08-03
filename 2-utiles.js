e1e0658c7e729a0c9b98e74a2e2c0cb8e317cc3b
Ibtissam <btissam1234btissam12@gmail.com>
2023-08-03 13:51:11 +0100

    modules defenation

---
+++ 2-utiles.js
@@ -0,0 +1,5 @@
+function sayhello(name){
+    console.log(`Hello ,this is ${name}`)
+}
+sayhello("Abdo")
+module.exports=sayhello;
---
+++ 3-module.js
@@ -0,0 +1,5 @@
+const names=require('./1-names');
+// console.log(require('./1-names').bassoma)
+const sayhello=require('./2-utiles')
+console.log(names.multipNames[0])
+sayhello(names.latofa)
