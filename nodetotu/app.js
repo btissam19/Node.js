a57e8e1db0795b90041969b86530d8f64b66d302
Ibtissam <btissam1234btissam12@gmail.com>
2023-08-16 18:25:05 +0100

    post and put methode

--- nodetotu/app.js
+++ nodetotu/app.js
@@ -39,17 +39,48 @@ app.get('/api/people', (req, res) => {
 })
 
 // POST route for /api/people
-app.post('/api/people', authorize, (req, res) => {
+app.post('/api/people', (req, res) => {
   const { name } = req.body
-  if (!name) {
-    return res.status(400).json({ success: false, msg: 'Please provide name value' })
+  if (name) {
+  
+    res.status(201).json({ success: true, person: name })
   }
-  res.status(201).json({ success: true, people: name })
+  return res.status(400).json({ success: false, msg: 'Please provide name value' })
 })
 
 // User route
 app.get('/user', authorize, (req, res) => {
   res.status(200).send("Welcome John")
+})
+
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).json({ success: true, data: [...people, name] })
+})
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+   console.log(id,name)
+   console.log("hello world")
+  const person = people.find((person) => person.id === Number(id))
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
 })
 
 const PORT = 3001
